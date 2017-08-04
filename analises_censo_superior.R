60f58d25ef0d866cab7bcefa5cc9d936c9ab9e3d
Neylson Crepalde <neylsoncrepalde@gmail.com>
2017-08-04 15:15:43 -0300

    Create analises_censo_superior.R

---
+++ analises_censo_superior.R
@@ -0,0 +1,39 @@
+########################################
+# Análises do Censo da Educação Superior
+# Prof. Neylson Crepalde
+########################################
+
+library(bit64)
+library(data.table)
+library(dplyr)
+library(descr)
+library(readr)
+
+setwd("~/microdados_censo_superior_2015/DADOS")
+list.files()
+
+aluno = fread("censo_superior_mg_2015_alunos.csv", encoding = "UTF-8")
+gc()
+
+freq(aluno$NO_IES, plot=F)
+freq(aluno$NO_CURSO, plot=F)
+
+musica_so = aluno %>% filter(NO_CURSO == "MÚSICA")
+musica_lic = aluno %>% filter(NO_CURSO == "MÚSICA - LICENCIATURA OU BACHARELADO")
+
+musica = rbind(musica_so, musica_lic)
+rm(musica_lic, musica_so)
+
+freq(musica$NO_IES, plot=F)
+
+index = grep("MÚSICA", aluno$NO_CURSO)
+
+musica_geral = aluno[index,]
+freq(musica_geral$NO_IES, plot=F)
+
+
+freq(musica$CO_CURSO, plot=F)
+# Juntando com base de cursos
+# Chave: CO_CURSO
+
+#curso = fread("censo_superior_mg_2015_curso.csv", encoding = "UTF-8")
